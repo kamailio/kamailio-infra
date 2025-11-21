01b0f15bdee2feb35823797c47dab8007347bf78
Victor Seva <linuxmaniac@torreviejawireless.org>
2025-11-21 10:57:27 +0100

    tf: fix output

--- environments/development/_outputs.tf
+++ environments/development/_outputs.tf
@@ -1,4 +1,4 @@
 output "jenkins-public-ips" {
-  value       = [for instance in aws_instance.jenkins : instance.public_ip]
+  value       = aws_instance.jenkins.public_ip
   description = "Jenkins public IP"
 }
